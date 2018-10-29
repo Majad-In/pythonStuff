3afc75a0ba5966d377597c0b41ede84e8c4b3b69
Majad-In <bobbyg.jones8@gmail.com>
2018-10-29 20:22:46 +0100

    first

---
+++ app.py
@@ -0,0 +1,59 @@
+
+# # this is a Int
+# students_count = 1000
+# # this is float
+# rating = 4.99
+# # boolean
+# is_published = True
+# # String
+
+# print(len(course))
+# print(course[0])
+# print(course[-1])
+# print(course[0:3])
+# print(course[0:])
+
+# course = "Python \nProgramming"
+# print(course)
+
+# first = "Bobby"
+# last = "Jones"
+# full = first + " " + last
+# full = f"{len(first)} {2 + 2}"
+# print(full)
+
+# course = "    python programming"
+# print(course.lower())
+# print(course.title())
+# print(course.strip())
+# print(course.find("pro"))
+# print(course.replace("p", "j"))
+# print("love" in course)  # boolean
+# print("python" not in course)  # boolean
+
+
+# print(10 + 3) addition
+# print(10 - 3) subtraction
+# print(10 * 3) multiplication
+# print(10 / 3) division
+# print(10 // 3)
+# print(10 % 3) # modulus operator
+# print(10 ** 5)  # last zeros
+
+# x = 10
+# x = x + 3  # same
+# x += 3  # same
+
+
+# import math
+
+# print(round(2.9))
+# print(abs(-2.9))
+# print(math.ceil(2.2))
+# print(math.copysign(3.0, -0.0))
+
+# x = input("x: ")
+# print(type(x))
+
+fruit = "Apple"
+print(fruit[1:-1])
